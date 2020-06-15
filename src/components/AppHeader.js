3fa13d273e652ddcd8a32fe687a0978c2ca48b4f
Vikram R <vikramrengarajan@gmail.com>
2020-06-15 01:06:34 -0500

    Update AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,8 +3,8 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip code</p>
+        <p className="p-3 text-light h3 bg-primary">Current weather by zip code - Running on IBM Cloud</p>
     </div>
 }
-  
+
 export default AppHeader
